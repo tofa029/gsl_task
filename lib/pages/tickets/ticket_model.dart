04198d286d0468c45fe0ecccf01e92f9c0c68f50
tofa029 <74099596+tofa029@users.noreply.github.com>
2025-04-21 10:23:39 +0600

    Update ticket_model.dart

--- lib/pages/tickets/ticket_model.dart
+++ lib/pages/tickets/ticket_model.dart
@@ -4,7 +4,7 @@ import './index.dart';
 class TicketModel{
   RxList<TicketEntity> tickets = <TicketEntity>[].obs;
 
-  RxList<String> brandListFromApi = <String>['Gains','GainsSolution','GainHQ','ITDesk','LTDHQ','Other'].obs;
+  RxList<String> brandListFromApi = <String>['Gainer','Ruler','Dmart','ITDesk','LTDHQ','Other'].obs;
   RxList<String> priorityListFromApi = <String>['Select Priority','Blocker','Urgent','High','Medium','Low'].obs;
   RxList<String> tagsListFromApi = <String>['Tag One','Tag two','Tag three long long text text','Tag four','Tag Five Simple','Tag Six long long text text','More and More tags','Tag Last'].obs;
 
